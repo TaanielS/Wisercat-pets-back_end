dce803fd4385a150546f7c58760fc5d7f73ca565
saarnik <saarnik@ut.ee>
2023-05-08 19:45:00 +0300

    Removed unused imports.

--- src/main/java/com/saarnik/pet/pet_management/pets/service/PetService.java
+++ src/main/java/com/saarnik/pet/pet_management/pets/service/PetService.java
@@ -2,8 +2,6 @@ package com.saarnik.pet.pet_management.pets.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
